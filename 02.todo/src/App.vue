06a4df34e9147d0770a34ef53ad49b75b49c67c6
Joran Quinten <joran.quinten@jumbo.com>
2023-09-06 16:50:45 +0200

    feat(chapter 2): add todo example app

---
+++ 02.todo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <AppHeader />
+  <TodoList />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AppHeader from './components/AppHeader.vue'
+import TodoList from './components/TodoList.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    AppHeader,
+    TodoList
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-size: 1.2rem;
+  min-height: 100vh;
+  background-color: #f0f0f0;
+}
+</style>
