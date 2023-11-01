0de962d6dbea89fa413cda2bc30a2950e1eadf8d
Joran Quinten <joran.quinten@jumbo.com>
2023-11-01 15:51:34 +0100

    feat(10-portfolio): adds notes

---
+++ 10.portfolio/.notes/10.1-Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="w-full py-8 mb-4 border-b-2">
+    <div class="container flex items-center justify-between h-full mx-auto">
+      <ULink
+        to="/"
+        class="font-bold"
+        active-class="text-primary"
+        inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700"
+        >🏡 Home</ULink
+      >
+      <nav>
+        <ul class="flex space-x-8 text-lg font-bold">
+          <li>
+            <ULink
+              to="/portfolio"
+              active-class="text-primary"
+              inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700"
+              >🗂️ Portfolio</ULink
+            >
+          </li>
+          <li>
+            <ULink
+              to="/me"
+              active-class="text-primary"
+              inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700"
+              >🧑‍💻 Me</ULink
+            >
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
---
+++ 10.portfolio/.notes/10.2-Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-green-100">
+    <div class="container flex items-center justify-between p-4 py-8 mx-auto">
+      <div>
+        Made with ❤️ using
+        <ULink
+          to="https://www.nuxt.com/"
+          inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700"
+          >NuxtJs</ULink
+        >
+        &amp;
+        <ULink
+          to="https://www.storyblok.com/"
+          inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700"
+          >Storyblok</ULink
+        >
+      </div>
+      <div>Copyright {{ new Date().getFullYear() }}</div>
+    </div>
+  </div>
+</template>
